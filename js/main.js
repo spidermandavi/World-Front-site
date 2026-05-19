f02beb5a0cefed42acf22752986621b607d22361
Rank-8_RK <djcraes28@gmail.com>
2026-05-19 12:45:14 +0200

    Add back and forward navigation functions

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
+
+function goBack() {
+  window.history.back();
+}
+
+function goForward() {
+  window.history.forward();
+}
